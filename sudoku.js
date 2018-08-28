6aa20e38efeb55aad49adf21bbfb7ab33d385d53
Kamen <kamen.boev@gmail.com>
2018-08-28 13:18:45 +0300

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -1,4 +1,36 @@
-const uploadImage = () => {
+const getURL = () => {
+	let imgURL = document.querySelectorAll("input")[0].value;
+	console.log(imgURL);
+	let container = document.getElementsByClassName("img-container")[0];
+	if(imgURL.length) {
+		let newImg = document.createElement('img');
+		
+		let att = document.createAttribute("src");
+		att.value = imgURL.toString();
+		newImg.setAttributeNode(att);
+		
+		let attHeight = document.createAttribute("height");
+		attHeight.value = "200px";
+		newImg.setAttributeNode(attHeight);
+		
+		let attWidth = document.createAttribute("width");
+		attWidth.value = "250px";
+		newImg.setAttributeNode(attWidth);
+		
+		let attId = document.createAttribute("id");
+		attId.value = "myImg" + 5;
+		newImg.setAttributeNode(attId);
+		
+		let attClick = document.createAttribute("onClick");
+		attClick.value = "getId(this.id);";
+		newImg.setAttributeNode(attClick);
+		
+		let attOrigin = document.createAttribute("crossOrigin");
+		attOrigin.value = "";
+		newImg.setAttributeNode(attOrigin);
+		
+		container.appendChild(newImg);
+	} 
 	
 }
 
@@ -7,10 +39,11 @@ let modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 let img = '';
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
+let addBtn = document.getElementById("add-btn");
 const getId = (id) => {
-	var myImage = document.getElementById(id);
+	let myImage = document.getElementById(id);
 	myImage.setAttribute('crossOrigin', '');
 
 	if(myImage.height === 0) {
@@ -23,8 +56,10 @@ const getId = (id) => {
 	
 	modal.style.display = "block";
 	captionText.innerHTML = "Loading, please wait.";
+	addBtn.style.zIndex = "-1";
 
-	Tesseract.recognize(myImage, {
+	
+	esseract.recognize(myImage, {
 		lang: 'eng'
 	})
 	.then(function(result){
@@ -32,13 +67,7 @@ const getId = (id) => {
 		modalImg.src = myImage.src;
 		captionText.innerHTML = result.text;
 	})
-	
-	// img = document.getElementById(id);
-	
-	modal.style.display = "block";
-    modalImg.src = myImage.src;
-    
-    //console.log(modal.style.display, modalImg, captionText.innerHTML)
+
 	
 }
 
@@ -47,4 +76,5 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
   modal.style.display = "none";
+  addBtn.style.zIndex = "1";
 }
